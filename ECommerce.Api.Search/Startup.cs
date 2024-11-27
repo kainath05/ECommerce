8e6bbd4f08bd02c91baa88faec2c56b2e48dfb4b
kainath Ahmed <2268774@johnabbottcollege.net>
2024-11-27 14:31:39 -0500

    added polly, postman search still not working

--- ECommerce.Api.Search/Startup.cs
+++ ECommerce.Api.Search/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Polly;
 
 namespace ECommerce.Api.Search
 {
@@ -36,7 +37,7 @@ namespace ECommerce.Api.Search
             services.AddHttpClient("ProductsService", config =>
             {
                 config.BaseAddress = new Uri(Configuration["Services:Products"]);
-            });
+            }).AddTransientHttpErrorPolicy(p=> p.WaitAndRetryAsync(5,_ => TimeSpan.FromMilliseconds(500)));
             services.AddControllers();
         }
 
